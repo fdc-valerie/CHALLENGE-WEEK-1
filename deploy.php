82d2ab27a3d2347f856df7505e855c36af6c91cf
fdc-valerie <fdc-valerie@github.com>
2017-10-20 01:39:57 +0000

    modify deploy indention

--- deploy.php
+++ deploy.php
@@ -29,16 +29,16 @@
 
 <!DOCTYPE HTML>
 <html lang="en-US">
-<head>
-    <meta charset="UTF-8">
-    <title>GIT DEPLOYMENT SCRIPT</title>
-</head>
-<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
-<div style="width:700px">
-    <div style="float:left;width:350px;">
-    <p style="color:white;">Git Deployment Script</p>
-    <?php echo $output; ?>
-    </div>
-</div>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <title>GIT DEPLOYMENT SCRIPT</title>
+    </head>
+    <body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
+        <div style="width:700px">
+            <div style="float:left;width:350px;">
+            <p style="color:white;">Git Deployment Script</p>
+            <?php echo $output; ?>
+            </div>
+        </div>
+    </body>
 </html>
