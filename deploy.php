19684165b7fc872be8967935655b04d7cca7c420
fdc.valerie <31604787+fdc-valerie@users.noreply.github.com>
2017-10-20 10:12:37 +0800

    modify na

--- deploy.php
+++ deploy.php
@@ -1,51 +1,4 @@
-<<<<<<< HEAD
-<?php
-    /**
-     * GIT DEPLOYMENT SCRIPT
-     *
-     * Used for automatically deploying websites via GitHub
-     *
-     */
 
-    // array of commands
-    $commands = array(
-        'echo $PWD',
-        'whoami',
-        'git pull',
-        'git status',
-        'git submodule sync',
-        'git submodule update',
-        'git submodule status',
-    );
-
-    // exec commands
-    $output = '';
-    foreach($commands AS $command){
-        $tmp = shell_exec($command);
-        
-        $output .= "<span style=\"color: #6BE234;\">\$</span><span style=\"color: #729FCF;\">{$command}\n</span><br />";
-        $output .= htmlentities(trim($tmp)) . "\n<br /><br />";
-        //comment
-    }
-?>
-
-<!DOCTYPE HTML>
-<html lang="en-US">
-<head>
-    <meta charset="UTF-8">
-    <title>GIT DEPLOYMENT SCRIPT</title>
-</head>
-<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
-<div style="width:700px">
-    <div style="float:left;width:350px;">
-    <p style="color:white;">Git Deployment Script bana</p>
-    <?php echo $output; ?>
-    </div>
-</div>
-</body>
-</html>
-
-=======
 <?php
     /**
      * GIT DEPLOYMENT SCRIPT
@@ -90,4 +43,4 @@
         </div>
     </body>
 </html>
->>>>>>> 82d2ab27a3d2347f856df7505e855c36af6c91cf
+
